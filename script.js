131b5e69559cca1a9aba49832f4b33e88cbe84f0
Gaviru Fernando <imgavirufdo@gmail.com>
2023-05-12 19:55:37 +0530

    files added

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+function productOpen(){
+  document.getElementById("productsingle").style.display="block";
+  document.getElementById("hide").style.display="none";
+  document.getElementById("hide2").style.display="none";
+  document.getElementById("myTopnav").style.display="none";
+  document.getElementById("hide3").style.display="none";
+  document.getElementById("hide4").style.display="none";
+  document.getElementById("hide5").style.display="none";
+  document.getElementById("hide6").style.display="none";
+  document.getElementById("hide7").style.display="none";
+}
+function unhide(){
+  document.getElementById("productsingle").style.display="none";
+  document.getElementById("hide").style.display="block";
+  document.getElementById("hide2").style.display="inline-block";
+  document.getElementById("myTopnav").style.display="block";
+  document.getElementById("hide3").style.display="inline-block";
+  document.getElementById("hide4").style.display="inline-block";
+  document.getElementById("hide5").style.display="inline-block";
+  document.getElementById("hide6").style.display="inline-block";
+  document.getElementById("hide7").style.display="inline-block";
+}
